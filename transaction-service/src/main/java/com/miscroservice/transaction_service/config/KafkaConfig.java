4a7a3e3dfc084e2c89b9706839ff13effaded733
Vladysalv Moskalenko <vladkiew15@gmail.com>
2025-03-20 23:14:50 +0100

    [improve] Set up kafka for consume document statuses

--- transaction-service/src/main/java/com/miscroservice/transaction_service/config/KafkaConfig.java
+++ transaction-service/src/main/java/com/miscroservice/transaction_service/config/KafkaConfig.java
@@ -2,13 +2,14 @@ package com.miscroservice.transaction_service.config;
 
 import com.miscroservice.transaction_service.model.dto.TransactionItemDto;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -44,4 +45,19 @@ public class KafkaConfig {
         factory.setConcurrency(3); // Adjust based on load
         return factory;
     }
+
+    @Bean
+    public ProducerFactory<String, String> feedbackProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> feedbackKafkaTemplate() {
+        return new KafkaTemplate<>(feedbackProducerFactory());
+    }
+
 }
