d4c8a931657bc02fd3aa8f1d6f5d72f36394817b
Vladysalv Moskalenko <vladkiew15@gmail.com>
2025-03-23 14:28:01 +0100

    init classification service

---
+++ classification-service/src/main/java/com/microservice/classification_service/ClassificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.classification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassificationServiceApplication.class, args);
+	}
+
+}
