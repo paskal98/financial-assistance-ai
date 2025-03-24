8d0e519c85670fa585ca808a881713618a36516b
Vladysalv Moskalenko <vladkiew15@gmail.com>
2025-03-24 10:12:34 +0100

    [refactor] Add shared lib. fix dependencies

--- shared-lib/src/main/java/org/shared/dto/FeedbackMessage.java
+++ shared-lib/src/main/java/org/shared/dto/FeedbackMessage.java
@@ -1,10 +1,14 @@
 package org.shared.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FeedbackMessage {
     private String documentId;
     private String stage; // "OCR", "CLASSIFICATION", "TRANSACTION"
