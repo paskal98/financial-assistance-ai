97f0f28444c03afb3b836becb34dfaf207620990
Vladysalv Moskalenko <vladkiew15@gmail.com>
2025-03-27 22:54:33 +0100

    [fix] remove PDF report generation

--- report-service/src/main/java/com/microservice/report_service/service/impl/ReportServiceImpl.java
+++ report-service/src/main/java/com/microservice/report_service/service/impl/ReportServiceImpl.java
@@ -3,20 +3,16 @@ package com.microservice.report_service.service.impl;
 import com.microservice.report_service.exception.ReportGenerationException;
 import com.microservice.report_service.model.dto.*;
 import com.microservice.report_service.model.entity.ReportMetadata;
-import com.microservice.report_service.model.entity.ReportSummary;
 import com.microservice.report_service.repository.ReportMetadataRepository;
 import com.microservice.report_service.security.JwtUtil;
 import com.microservice.report_service.service.ReportService;
-import com.microservice.report_service.service.analytics.ReportAnalyticsService;
 import com.microservice.report_service.service.client.TransactionClient;
 import com.microservice.report_service.util.CsvReportGenerator;
-import com.microservice.report_service.util.PdfReportGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -24,10 +20,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ReportServiceImpl implements ReportService {
 
-    private final ReportAnalyticsService analyticsService;
     private final TransactionClient transactionClient;
     private final ReportMetadataRepository metadataRepository;
-    private final PdfReportGenerator pdfReportGenerator;
     private final CsvReportGenerator csvReportGenerator;
     private final JwtUtil jwtUtil;
 
@@ -36,7 +30,6 @@ public class ReportServiceImpl implements ReportService {
         try {
             String jwtToken = SecurityContextHolder.getContext().getAuthentication().getCredentials().toString();
 
-            // Получение текущих транзакций
             PagedTransactionResponse thisPeriod = transactionClient.getTransactions(
                     "Bearer " + jwtToken,
                     request.getStartDate().toString(),
@@ -48,40 +41,11 @@ public class ReportServiceImpl implements ReportService {
             );
             List<TransactionResponse> currentTransactions = thisPeriod.getContent();
 
-            // Определим предыдущий период по тем же датам
-            Instant start = request.getStartDate();
-            Instant end = request.getEndDate();
-
-            long days = java.time.Duration.between(start, end).toDays() + 1;
-
-            Instant prevStart = start.minus(java.time.Duration.ofDays(days));
-            Instant prevEnd = end.minus(java.time.Duration.ofDays(days));
-
-
-            // Получение транзакций за предыдущий период
-            PagedTransactionResponse lastPeriod = transactionClient.getTransactions(
-                    "Bearer " + jwtToken,
-                    prevStart.toString(),
-                    prevEnd.toString(),
-                    request.getCategory(),
-                    request.getType(),
-                    0,
-                    1000
-            );
-            List<TransactionResponse> previousTransactions = lastPeriod.getContent();
-
-            ReportSummary summary = analyticsService.analyze(currentTransactions, previousTransactions);
-
-            String fileUrl = switch (request.getFormat().toLowerCase()) {
-                case "pdf" -> pdfReportGenerator.generateReport(currentTransactions, request, summary);
-                case "csv" -> csvReportGenerator.generateReport(currentTransactions, request);
-                default -> throw new ReportGenerationException("Unsupported report format: " + request.getFormat());
-            };
+            String fileUrl = csvReportGenerator.generateReport(currentTransactions, request);
 
             ReportMetadata metadata = new ReportMetadata();
             metadata.setUserId(UUID.fromString(jwtUtil.extractUserId(token.substring(7))));
             metadata.setFileUrl(fileUrl);
-            metadata.setFormat(request.getFormat());
             metadata.setGeneratedAt(Instant.now());
             metadata.setStartDate(request.getStartDate());
             metadata.setEndDate(request.getEndDate());
